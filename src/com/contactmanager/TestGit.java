66c4c0b105ded47cf41b5e8529ba9fb99c63d936
matthew W <bwan210@aucklanduni.ac.nz>
2013-10-25 08:06:40 +1300

    added code;

--- src/com/contactmanager/TestGit.java
+++ src/com/contactmanager/TestGit.java
@@ -2,4 +2,12 @@ package com.contactmanager;
 
 public class TestGit {
 
+		public static void main(String[] args){
+			System.out.println("there is no git");
+			System.out.print("test");
+		}
+		
+		public static int test(){
+			return 2;
+		}
 }
